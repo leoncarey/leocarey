798937d70a1f25670d4aa4bd748cb8bf70963db1
Leonardo Carey <careycoder@gmail.com>
2021-08-07 00:10:36 -0300

    CSS updated

--- src/App.js
+++
@@ -1,9 +0,0 @@
-
-
-function App() {
-  return (
-    <div>lalal</div>
-  );
-}
-
-export default App;
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Container } from './styles'
+
+import './theme.css'
+
+function App() {
+  return (
+    <Container>
+      Inner site
+    </Container>
+  );
+}
+
+export default App;
