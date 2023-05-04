546477d088ba5f81590e46ee56b56ccbcdaf5204
Leo Carey <careycoder@gmail.com>
2023-05-04 20:57:45 -0300

    feat: site updated to vue

--- src/sections/About/styles.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components'
-
-export const Container = styled.section`
-    display: block;
-    width: 700px;
-    height: 1200px;
-    position: relative;
-    margin-top: 120px;
-`
--- src/sections/general-styles.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components'
-
-export const MainSection = styled.section`
-    position: relative;
-    width: 100%;
-    display: block;
-    margin-bottom: 800px;
-`
